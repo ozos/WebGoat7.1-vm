fb5caa4da74eef6401de6e0354432141e9afe916
Luis Leyva <ozos.leyher@gmail.com>
2017-05-07 19:23:26 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+curl -O https://s3.amazonaws.com/webgoat-war/webgoat-container-7.1-SNAPSHOT-war-exec.jar
+
+java -jar webgoat-container-7.1-SNAPSHOT-war-exec.jar
+
+
+echo "WebGoat installed"
